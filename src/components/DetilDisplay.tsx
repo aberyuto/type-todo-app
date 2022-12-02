6b13ebff551a47b7331fcd6f087cb83c0b87dc39
Ryoga <te190748@gmail.com>
2022-12-02 14:08:25 +0900

    カレンダー機能追加

--- src/components/DetilDisplay.tsx
+++ src/components/DetilDisplay.tsx
@@ -1,20 +1,24 @@
-import { Component } from "react";
+import { Component, useContext } from "react";
 import type { AppProps } from "next/app";
 import { TodoContext } from "./providers/TodoProvider";
 import Link from "next/link";
 import { Task } from "../../pages/todo";
 import { Button } from "@mantine/core";
+import { SimpleDatePicker } from "./Calendar";
+import { DateContext } from "../../pages/_app";
 
 type Props = {
   task: Task;
 };
 
 export default function DeatilDisplay(props: Props) {
+  const { startDate, setStartDate } = useContext(DateContext);
   return (
     <div>
       <h3>{props.task.text}</h3>
       <p>コンテンツの詳細</p>
       <p>内容</p>
+      <SimpleDatePicker startDate={startDate} setStartDate={setStartDate} />
       <br></br>
       <Button variant="outline" color="indigo">
         <Link href="/todo">戻る</Link>
